52807fba24f5c0ce066ba7d7ed47db8c53960389
Adrian Smijulj <adrian1358@gmail.com>
2020-07-03 14:38:27 +0200

    fix: remove validators setup

--- packages/app-headless-cms/src/admin/components/ContentModelForm/index.tsx
+++ packages/app-headless-cms/src/admin/components/ContentModelForm/index.tsx
@@ -1,13 +1,8 @@
 import React from "react";
-import { I18NValue } from "@webiny/app-i18n/components";
-import { getPlugins } from "@webiny/plugins";
 import { cloneDeep, pick } from "lodash";
 import { ContentFormRender } from "./ContentFormRender";
 
-import {
-    CmsContentModelFormProps,
-    CmsFormFieldValidatorPlugin
-} from "@webiny/app-headless-cms/types";
+import { CmsContentModelFormProps } from "@webiny/app-headless-cms/types";
 
 export const ContentModelForm: React.FC<CmsContentModelFormProps> = props => {
     const { contentModel: contentModelRaw } = props;
@@ -28,47 +23,9 @@ export const ContentModelForm: React.FC<CmsContentModelFormProps> = props => {
             returnFields = [...fields.map(item => [item._id])];
         }
 
-        const validatorPlugins: CmsFormFieldValidatorPlugin[] = getPlugins("form-field-validator");
-
         returnFields.forEach(row => {
             row.forEach((id, idIndex) => {
                 row[idIndex] = getFieldById(id);
-
-                if (Array.isArray(row[idIndex].validation)) {
-                    row[idIndex].validators = row[idIndex].validation
-                        .map(item => {
-                            const validatorPlugin = validatorPlugins.find(
-                                plugin => plugin.validator.name === item.name
-                            );
-
-                            if (
-                                !validatorPlugin ||
-                                typeof validatorPlugin.validator.validate !== "function"
-                            ) {
-                                return;
-                            }
-
-                            return async value => {
-                                let isInvalid;
-                                try {
-                                    const result = await validatorPlugin.validator.validate(
-                                        value,
-                                        item
-                                    );
-                                    isInvalid = result === false;
-                                } catch (e) {
-                                    isInvalid = true;
-                                }
-
-                                if (isInvalid) {
-                                    throw new Error(
-                                        I18NValue({ value: item.message }) || "Invalid value."
-                                    );
-                                }
-                            };
-                        })
-                        .filter(Boolean);
-                }
             });
         });
 
