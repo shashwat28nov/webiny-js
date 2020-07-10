76ed7a76a5a1c7db226bc0ce268da7d3607b23fa
Ashutosh <bhardwajashu96@hotmail.com>
2020-07-10 11:54:12 +0530

    feat(api-headless-cms): update `createValidation` function
    update `createValidation` to support `multipleValuesValidation`

--- packages/api-headless-cms/src/content/plugins/utils/createValidation.ts
+++ packages/api-headless-cms/src/content/plugins/utils/createValidation.ts
@@ -1,9 +1,40 @@
 import {
     CmsContext,
     CmsContentModelField,
-    CmsModelFieldValidatorPlugin
+    CmsModelFieldValidatorPlugin,
+    CmsFieldValidation
 } from "@webiny/api-headless-cms/types";
 import { getI18NValue } from "./getI18NValue";
+
+type RunValidationParams = {
+    plugins: CmsModelFieldValidatorPlugin[];
+    validations: CmsFieldValidation[];
+    value: any;
+    context: CmsContext;
+};
+
+const runValidations = async (params: RunValidationParams) => {
+    const { validations, plugins, value, context } = params;
+    for (let i = 0; i < validations.length; i++) {
+        const validator = validations[i];
+        const validatorPlugin = plugins.find(plugin => plugin.validator.name === validator.name);
+
+        if (!validatorPlugin || typeof validatorPlugin.validator.validate !== "function") {
+            continue;
+        }
+
+        let valid = false;
+        try {
+            valid = await validatorPlugin.validator.validate({ value, validator, context });
+        } catch (e) {
+            valid = false;
+        }
+
+        if (!valid) {
+            throw new Error(getI18NValue(validator.message) || "Invalid value.");
+        }
+    }
+};
 
 export const createValidation = (field: CmsContentModelField, context: CmsContext) => {
     const plugins = context.plugins.byType<CmsModelFieldValidatorPlugin>(
@@ -11,28 +42,28 @@ export const createValidation = (field: CmsContentModelField, context: CmsContex
     );
 
     return async value => {
-        for (let i = 0; i < field.validation.length; i++) {
-            const validator = field.validation[i];
-            const validatorPlugin = plugins.find(
-                plugin => plugin.validator.name === validator.name
-            );
-
-            if (!validatorPlugin || typeof validatorPlugin.validator.validate !== "function") {
-                continue;
+        if (field.multipleValues) {
+            await runValidations({
+                validations: field.multipleValuesValidation,
+                value,
+                plugins,
+                context
+            });
+            // Run field validation for each value item in case of `multipleValues = true`
+            for (let index = 0; index < value.length; index++) {
+                const currentValue = value[index];
+                await runValidations({
+                    validations: field.validation,
+                    value: currentValue,
+                    plugins,
+                    context
+                });
             }
 
-            let valid = false;
-            try {
-                valid = await validatorPlugin.validator.validate({ value, validator, context });
-            } catch (e) {
-                valid = false;
-            }
-
-            if (!valid) {
-                throw new Error(getI18NValue(validator.message) || "Invalid value.");
-            }
+            return true;
+        } else {
+            await runValidations({ validations: field.validation, value, plugins, context });
+            return true;
         }
-
-        return true;
     };
 };
